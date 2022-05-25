852520bab52b8f3c51aedd99c6c6e1ad940107ae
Sweet4ever <oskpil@gmail.com>
2022-05-25 16:52:22 +0200

    index file and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { LoginComponent } from './login/login.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    LoginComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
