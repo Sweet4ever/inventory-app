c5ae1d94796ccb0cb0557b576bb18291580115b7
Sweet4ever <oskpil@gmail.com>
2022-06-02 22:50:31 +0200

    add update to item

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -21,4 +21,37 @@ export class ItemComponent {
     this.angularFirestore.collection("items").doc(item.id).delete();
     alert("item deleted")
   }
+  
+  showEdit(item:any){
+    let name = <HTMLInputElement>document.querySelector(`#${item.id} #itemName`);
+    let amount = <HTMLInputElement>document.querySelector(`#${item.id} #itemAmount`);
+    let unit = <HTMLInputElement>document.querySelector(`#${item.id} #itemUnit`);
+
+    document.querySelector(`#${item.id} #info`)?.classList.add("hidden");
+    document.querySelector(`#${item.id} #info`)?.classList.remove("show");
+    document.querySelector(`#${item.id} #editItem`)?.classList.add("show")
+    document.querySelector(`#${item.id} #editItem`)?.classList.remove("hidden");
+
+    name.value = item.name;
+    amount.value = item.amount;
+    unit.value = item.unit;
+  }
+
+  updateItem(item:any){
+    let name = <HTMLInputElement>document.querySelector(`#${item.id} #itemName`);
+    let amount = <HTMLInputElement>document.querySelector(`#${item.id} #itemAmount`);
+    let unit = <HTMLInputElement>document.querySelector(`#${item.id} #itemUnit`);
+
+    document.querySelector(`#${item.id} #info`)?.classList.add("show");
+    document.querySelector(`#${item.id} #info`)?.classList.remove("hidden");
+    document.querySelector(`#${item.id} #editItem`)?.classList.add("hidden")
+    document.querySelector(`#${item.id} #editItem`)?.classList.remove("show");
+    
+    this.angularFirestore.collection("items").doc(item.id).set({
+      name: name.value,
+      amount: amount.value,
+      unit: unit.value,
+      category: item.category
+    })
+  }
 }
